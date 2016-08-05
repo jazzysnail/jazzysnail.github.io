2b4a6b82ebd2e11eaf2e802a06cecd6be9b6814e
guoliang <guoliang@danlu.com>
2016-08-05 15:45:39 +0800

    Site updated: 2016-08-05 15:45:39

--- js/start.js
+++ js/start.js
@@ -1,3 +1,3 @@
-particlesJS.load('bge', 'http://oaul0t4t1.bkt.clouddn.com/e_particles.json', function() {
+particlesJS.load('bge', 'http://oaul0t4t1.bkt.clouddn.com/bge_cfg.json', function() {
   console.log('callback - particles.js config loaded');
 });
