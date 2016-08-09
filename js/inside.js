b390fa61b6941a3909dc89eb14935185ba78c0fb
guoliang <guoliang@danlu.com>
2016-08-09 16:50:38 +0800

    Site updated: 2016-08-09 16:50:37

--- js/inside.js
+++ js/inside.js
@@ -1,4 +1,4 @@
 'use strict';
-particlesJS.load('bge', 'http://oaul0t4t1.bkt.clouddn.com/imgs/bge_config.json', function() {
+particlesJS.load('bge', '../json/bge_config.json', function() {
   console.log('particles effect is starting!');
 });
